5a0d3afad982f3c21e088c87665b5c66432fc523
Nolan Kovacik <noltron000@gmail.com>
2018-07-03 15:26:51 -0400

    Robustified

--- main.js
+++ main.js
@@ -1,6 +1,49 @@
 $(".single-item").slick({
-    slidesToShow: 1,
-    slidesToScroll: 1,
-    rows: 3,
-    variableWidth: true,
-});
+    dots: true,
+    infinite: true,
+    speed: 300,
+    slidesToShow: 6,
+    slidesToScroll: 6,
+    responsive: [{
+    },
+    {
+        breakpoint: 2160,
+        settings: {
+            slidesToShow: 4,
+            slidesToScroll: 4,
+            infinite: true,
+            dots: true
+        }
+    },
+    {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 3,
+          infinite: true,
+          dots: true
+        }
+    },
+    {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2,
+          infinite: true,
+          dots: false
+        }
+    },
+    {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+          infinite: true,
+          dots: false
+        }
+    }
+      // You can unslick at a given breakpoint now by adding:
+      // settings: "unslick"
+      // instead of a settings object
+    ]
+  });
