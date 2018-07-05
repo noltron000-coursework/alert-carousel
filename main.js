b3d60056a408c507ddf48b7696a4e34f1036bd08
Nolan Kovacik <noltron000@gmail.com>
2018-07-05 09:33:26 -0400

    Created two demonstrations of the carousel

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-$(".single-item").slick({
+$(".multi-row").slick({
     rows: 4,
     dots: true,
     infinite: true,
@@ -51,4 +51,55 @@ $(".single-item").slick({
       // settings: "unslick"
       // instead of a settings object
     ]
-  });
+});
+  
+$(".single-item").slick({
+    rows: 1,
+    dots: true,
+    infinite: true,
+    speed: 300,
+    slidesToShow: 6,
+    slidesToScroll: 6,
+    responsive: [{
+    },
+    {
+        breakpoint: 2160,
+        settings: {
+            slidesToShow: 4,
+            slidesToScroll: 4,
+            infinite: true,
+            dots: true
+        }
+    },
+    {
+        breakpoint: 1024,
+        settings: {
+            slidesToShow: 3,
+            slidesToScroll: 3,
+            infinite: true,
+            dots: true
+        }
+    },
+    {
+        breakpoint: 600,
+        settings: {
+            slidesToShow: 2,
+            slidesToScroll: 2,
+            infinite: true,
+            dots: false
+        }
+    },
+    {
+        breakpoint: 480,
+        settings: {
+            slidesToShow: 1,
+            slidesToScroll: 1,
+            infinite: true,
+            dots: false
+        }
+    }
+      // You can unslick at a given breakpoint now by adding:
+      // settings: "unslick"
+      // instead of a settings object
+    ]
+});
