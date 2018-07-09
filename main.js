9ebc968cabd6e24eb1be0dcd54b4dc9049e2f32c
Nolan Kovacik <noltron000@gmail.com>
2018-07-09 12:44:31 -0400

    Utilized CSS asset ~ object-fit

--- main.js
+++ main.js
@@ -1,65 +1,11 @@
-$(".multi-row").slick({
-    rows: 4,
-    dots: true,
-    infinite: true,
-    speed: 300,
-    slidesToShow: 6,
-    slidesToScroll: 6,
-    responsive: [{
-    },
-    {
-        breakpoint: 2160,
-        settings: {
-            rows: 3,
-            slidesToShow: 4,
-            slidesToScroll: 4,
-            infinite: true,
-            dots: true
-        }
-    },
-    {
-        breakpoint: 1024,
-        settings: {
-            rows: 2,
-            slidesToShow: 3,
-            slidesToScroll: 3,
-            infinite: true,
-            dots: true
-        }
-    },
-    {
-        breakpoint: 600,
-        settings: {
-            rows: 2,
-            slidesToShow: 2,
-            slidesToScroll: 2,
-            infinite: true,
-            dots: false
-        }
-    },
-    {
-        breakpoint: 480,
-        settings: {
-            rows: 1,
-            slidesToShow: 1,
-            slidesToScroll: 1,
-            infinite: true,
-            dots: false
-        }
-    }
-      // You can unslick at a given breakpoint now by adding:
-      // settings: "unslick"
-      // instead of a settings object
-    ]
-});
-  
 $(".single-item").slick({
     rows: 1,
-    dots: true,
-    infinite: true,
     speed: 300,
+
     slidesToShow: 6,
     slidesToScroll: 6,
+    infinite: true,
+    dots: true,
     responsive: [{
     },
     {
@@ -72,7 +18,7 @@ $(".single-item").slick({
         }
     },
     {
-        breakpoint: 1024,
+        breakpoint: 1280,
         settings: {
             slidesToShow: 3,
             slidesToScroll: 3,
@@ -81,7 +27,7 @@ $(".single-item").slick({
         }
     },
     {
-        breakpoint: 600,
+        breakpoint: 720,
         settings: {
             slidesToShow: 2,
             slidesToScroll: 2,
@@ -103,3 +49,6 @@ $(".single-item").slick({
       // instead of a settings object
     ]
 });
+
+// 2 - force slidesToScroll in centerMode
+$(".single-item").slick('slickSetOption', 'slidesToScroll', 3, true);
