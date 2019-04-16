362c511bb2959539757047000ec938a7c421d045
Nolan Kovacik <noltron000@gmail.com>
2019-04-16 16:54:11 -0700

    enforcing tab consistancy

--- main.js
+++ main.js
@@ -1,47 +1,47 @@
 $(".single-item").slick({
-    rows: 1,
-    speed: 300,
-    lazyLoad: 'ondemand',
-    slidesToShow: 6,
-    slidesToScroll: 6,
-    swipeAngleOffset: 60,
-    touchThreshold: 12,
-    infinite: true,
-    dots: false,
-    responsive: [{
-    },
-    {
-        breakpoint: 2100,
-        settings: {
-            slidesToShow: 4,
-            slidesToScroll: 4
-        }
-    },
-    {
-        breakpoint: 1800,
-        settings: {
-            slidesToShow: 3,
-            slidesToScroll: 3
-        }
-    },
-    {
-        breakpoint: 1500,
-        settings: {
-            slidesToShow: 2,
-            slidesToScroll: 2
-        }
-    },
-    {
-        breakpoint: 1200,
-        settings: {
-            slidesToShow: 1,
-            slidesToScroll: 1
-        }
-    }
-      // You can unslick at a given breakpoint now by adding:
-      // settings: "unslick"
-      // instead of a settings object
-    ]
+	rows: 1,
+	speed: 300,
+	lazyLoad: 'ondemand',
+	slidesToShow: 6,
+	slidesToScroll: 6,
+	swipeAngleOffset: 60,
+	touchThreshold: 12,
+	infinite: true,
+	dots: false,
+	responsive: [{
+	},
+	{
+		breakpoint: 2100,
+		settings: {
+			slidesToShow: 4,
+			slidesToScroll: 4
+		}
+	},
+	{
+		breakpoint: 1800,
+		settings: {
+			slidesToShow: 3,
+			slidesToScroll: 3
+		}
+	},
+	{
+		breakpoint: 1500,
+		settings: {
+			slidesToShow: 2,
+			slidesToScroll: 2
+		}
+	},
+	{
+		breakpoint: 1200,
+		settings: {
+			slidesToShow: 1,
+			slidesToScroll: 1
+		}
+	}
+		// You can unslick at a given breakpoint now by adding:
+		// settings: "unslick"
+		// instead of a settings object
+	]
 });
 
 // 2 - force slidesToScroll in centerMode
